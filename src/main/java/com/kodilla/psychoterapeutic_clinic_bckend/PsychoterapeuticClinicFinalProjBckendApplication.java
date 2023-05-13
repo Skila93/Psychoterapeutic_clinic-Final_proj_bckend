6aa1baec3364d8953ba01817c7aa35ba81954503
Skila93 <skalskikonrad@gmail.com>
2023-05-13 19:10:28 +0200

    First commit

---
+++ src/main/java/com/kodilla/psychoterapeutic_clinic_bckend/PsychoterapeuticClinicFinalProjBckendApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.psychoterapeutic_clinic_bckend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychoterapeuticClinicFinalProjBckendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsychoterapeuticClinicFinalProjBckendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodilla/psychoterapeutic_clinic_bckend/PsychoterapeuticClinicFinalProjBckendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.psychoterapeutic_clinic_bckend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsychoterapeuticClinicFinalProjBckendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
